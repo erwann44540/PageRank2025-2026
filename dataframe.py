9d731bf41d004babf4d713625054d7e82f1b5645
Quentin <quentin.tegny@etu.univ-nantes.fr>
2025-12-07 17:27:24 +0100

    Fix : code cleaning

--- dataframe.py
+++ dataframe.py
@@ -10,47 +10,42 @@ def run_pageRank_dataframe(conf, sc, spark) :
     appName = "MyApp"
     master = "local[*]"
 
-    def plus_mean(pandas_df):
-        return pandas_df.assign(_1=pandas_df._1 - pandas_df._1.mean())
-
     duration = []
-
     distFile = sc.textFile("20000_first_lines.ttl").map(lambda r : (Row(r.split(" ")[0]), Row(r.split(" ")[1]), Row(r.split(" ")[2]))).toDF()
 
     sel = distFile.select("_1","_3")
     sel = sel.withColumnRenamed("_3", "_2")
     distFile.createOrReplaceTempView("tableA")
-    mewtwo = spark.sql("SELECT tableA._1 as _1, 1/count(*) as _3 from tableA GROUP BY tableA._1")
-    tests = spark.sql("SELECT tableA._1 as _1, 1/count(*) as _3 from tableA GROUP BY tableA._1")
+    select_cmd = spark.sql("SELECT tableA._1 as _1, 1/count(*) as _3 from tableA GROUP BY tableA._1")
 
     start = time.time()
 
-    tests = tests.join(sel, "_1")
-    mew = tests
-    tests = tests.groupby("_2").sum("_3")
-    tests = tests.withColumnRenamed("sum(_3)", "_3")
-    tests = tests.withColumn('_3', 0.15+0.85*tests._3)
-    tests = tests.withColumnRenamed("_2", "_1")
+    select_cmd = select_cmd.join(sel, "_1")
+    temp_state = select_cmd
+    select_cmd = select_cmd.groupby("_2").sum("_3")
+    select_cmd = select_cmd.withColumnRenamed("sum(_3)", "_3")
+    select_cmd = select_cmd.withColumn('_3', 0.15+0.85*select_cmd._3)
+    select_cmd = select_cmd.withColumnRenamed("_2", "_1")
 
-    mew = mew.withColumnRenamed("_3", "_4")
+    temp_state = temp_state.withColumnRenamed("_3", "_4")
     duration.append(time.time() - start)
     spark.interruptAll()
     for i in range(1,5) :
         start = time.time()
-        tests = tests.join(mew, "_1")
-        print(tests.show())
-        tests = tests.withColumn('_3', tests._3*tests._4)
-        tests = tests.groupby("_2").sum("_3")
-        tests = tests.withColumnRenamed("sum(_3)", "_3")
-        tests = tests.withColumn('_3', 0.15+0.85*tests._3)
-        tests = tests.withColumnRenamed("_2", "_1")
+        select_cmd = select_cmd.join(temp_state, "_1")
+        print(select_cmd.show())
+        select_cmd = select_cmd.withColumn('_3', select_cmd._3*select_cmd._4)
+        select_cmd = select_cmd.groupby("_2").sum("_3")
+        select_cmd = select_cmd.withColumnRenamed("sum(_3)", "_3")
+        select_cmd = select_cmd.withColumn('_3', 0.15+0.85*select_cmd._3)
+        select_cmd = select_cmd.withColumnRenamed("_2", "_1")
         duration.append(time.time() - start)
         spark.interruptAll()
         sc.cancelAllJobs()
 
     print(duration)
     return duration
-    #suite = tests.join ("")
+    #suite = select_cmd.join ("")
     """print(sel.show())
     rank = sel.select("_1")
     rank = rank.withColumn('_3', lit(1))
--- rdd.py
+++ rdd.py
@@ -6,50 +6,16 @@ def run_pageRank_rdd(conf, sc) :
     master = "local[*]"
 
     distFile = sc.textFile("20000_first_lines.ttl")
-
-    #print(distFile.collect())
     dico = {}
+    map1 = distFile.map(lambda t : (t.split(" ")[0], [t.split(" ")[2]])).reduceByKey(lambda x,y : x+y)
 
-    def f(a):
-        key2 = a.split(" ")[0]
-        value2 = a.split(" ")[2]
-        if key in dico:
-            dico[key].append(value)
-            return a.split(" ")[0], a.split(" ")[2]
-        else:
-            dico[key] = [value]
-            return a.split(" ")[0], a.split(" ")[2]
-
-    def fqdklsmjf(x, y):
-        list = []
-        if x[0] == y[0] :
-            list = [x[1],y[1]]
-            return (x[0], list)
-        else :
-            return (x[0], x[1])
-
-    test2 = distFile.map(lambda t : (t.split(" ")[0], [t.split(" ")[2]])).reduceByKey(lambda x,y : x+y)
-    test4 = distFile.map(f)
-    test3 = distFile.map(lambda t : (t.split(" ")[0])).distinct()
-
-    #print(test2.collect())
-
-    for i in test2.collect():
+    for i in map1.collect():
         key = i[0]
         value = i[1]
         if key in dico:
             dico[key].append(value)
         else:
             dico[key] = [value]
-
-    #test4 = test2.join(test3)
-    def afficher_dico(dico):
-        cpt=1
-        for key, values in dico.items():
-            print(f"Clé {cpt} {key} :")
-            for value in values:
-                print(f"  - {value}")
-            cpt += 1
 
     def ecrire_dico(dico, filename):
         cpt=1
@@ -63,48 +29,29 @@ def run_pageRank_rdd(conf, sc) :
 
     ecrire_dico(dico, "avecdistinct")
 
-    def ecrire_map(liste, filename):
-        cpt=1
-        with open(filename, 'w', -1, "utf-8") as f:
-            for tpl1, liste_values in liste:
-                f.write(f"Tuple {cpt} - {tpl1} :\n")
-                for value in liste_values:
-                    f.write(f"  - {value}\n")
-                cpt += 1
-            f.write(f"Nombre total de clés : {len(liste)}")
+    triplets = distFile.flatMap(lambda t : (t.split(" ")[0], t.split(" ")[1], t.split(" ")[2])).distinct()
+    ranks = triplets.map(lambda node: (node, 1.0))
 
-    #ecrire_map(test2.collect(), 'map_output.txt')
-    #print(test4.collect())
-    test = distFile.flatMap(lambda t : (t.split(" ")[0], t.split(" ")[1], t.split(" ")[2])).distinct()
-
-    #print(test.collect())
-
-    ranks = test.map(lambda node: (node, 1.0))
-
-    #print (ranks.collect())
     def fusion(liste, nbr):
         ret = []
         for i in liste:
             ret.append((i,nbr))
         return ret
-    #ranks.saveAsTextFile('rank')
-    def func (tuples) :
+
+    def func(tuples) :
         urls = tuples[0]
         rank = tuples[1]
         lenurl = len(urls)
         dividedRank = rank/lenurl
         return fusion(urls,dividedRank)
+
     duration = []
     for i in range(1,6):
         start = time.time()
-        biboup = test2.join(ranks).values().flatMap(func)
-        #biboup = test2.join(ranks).values().flatMap(lambda urls,rank : (urls.map(lambda dest : (dest, rank/len(urls)))))
-        ranks = biboup.reduceByKey(lambda x,y : x+y).mapValues(lambda z : 0.15 + 0.85 * z)
+        flat_map = map1.join(ranks).values().flatMap(func)
+        ranks = flat_map.reduceByKey(lambda x,y : x+y).mapValues(lambda z : 0.15 + 0.85 * z)
         duration.append(time.time() - start)
         sc.cancelAllJobs()
-    iter = 5
+
     print(duration)
     return duration
-    #contribs = test2.join(ranks).values().flatMap(lambda case : case.map(lambda dest : (dest, case[1]/case[0].size())))
-
-    #print (contribs.collect())
